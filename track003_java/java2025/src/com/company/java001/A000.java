e2d75bd396d6b794ff97a4cc86db99e9f8ae6ff2
kangseohyun-s <ksgkey567@gmail.com>
2025-08-26 17:42:01 +0900

    day002.md

---
+++ track003_java/java2025/src/com/company/java001/A000.java
@@ -0,0 +1,12 @@
+package com.company.java001; // 1. 부품의 위치
+
+// 주석, 설명
+public class A000 { // 어디에서든지 접근 가능, 부품 객체, 부품 이름(클래스 이름)
+           public            static        void main(String[] args) {
+// 3. 어디에서든지 접근 가능, 바로 사용 가능 메모리,  void main(String[] args) 전원 버튼 이름
+        		  System.out.println("Hello Java");
+        	 // 4. 운영 체제 부품 객체. 출력(cmd). println("하고 싶은 말") 출력
+           }
+} // end class
+  
+// ctrl + f11 (실행)
---
+++ track003_java/java2025/src/com/company/java001_ex/A000_ex.java
@@ -0,0 +1,7 @@
+package com.company.java001_ex; // 경로 위치
+
+public class A000_ex {// 어디서든 접근 완료 , 부품 객체
+ public static void main(String[] args) {
+	 System.out.println("한 번 작성하면 어디서든 실행된다.");
+ }
+}
