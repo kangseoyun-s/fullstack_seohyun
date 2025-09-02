6e25a963d86d9d45e1d408aefcfbdeee87f80139
kangseohyun-s <ksgkey567@gmail.com>
2025-09-02 17:36:44 +0900

    java

---
+++ track003_java/java2025/src/com/company/java002_ex/InputEx2001.java
@@ -0,0 +1,15 @@
+package com.company.java002_ex;
+
+import java.util.Scanner;
+
+public class InputEx2001 {
+	public static void main(String[] args) {
+		double pi = 0.0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("파이 값을 입력하시오.");
+		pi  = scanner.nextDouble();
+		System.out.println("파이 값은" + pi + "입니다.");
+		
+	}
+
+}
