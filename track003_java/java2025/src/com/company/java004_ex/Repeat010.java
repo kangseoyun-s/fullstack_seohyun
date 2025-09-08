2e64299a61b30fa3267d3a088349c0e0d6e8c275
kangseohyun-s <ksgkey567@gmail.com>
2025-09-08 17:49:19 +0900

    day011

--- track003_java/java2025/src/com/company/java004_ex/Repeat010.java
+++ track003_java/java2025/src/com/company/java004_ex/Repeat010.java
@@ -2,7 +2,7 @@ package com.company.java004_ex;
 
 import java.util.Scanner;
 
-public class Repeat10 {
+public class Repeat010 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int num;
